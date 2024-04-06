2b9a9ec668b9448c5319f3b1ed13ed7b64357067
YaraBlack <yara26072000@gmail.com>
2024-04-06 16:13:23 +0300

    add an availability to create a comment

--- djangoProject/base/views.py
+++ djangoProject/base/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 # Create your views here.
@@ -76,7 +76,17 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {"room": room}
+    room_messages = room.message_set.all().order_by(
+        "-created"
+    )  # parent_model.child_model_*method*.*query*()
+
+    if request.method == "POST":
+        message = Message.objects.create(
+            user=request.user, room=room, body=request.POST.get("body")
+        )
+        return redirect("room", pk=room.id)
+
+    context = {"room": room, "room_messages": room_messages}
     return render(request, "base/room.html", context)
 
 
